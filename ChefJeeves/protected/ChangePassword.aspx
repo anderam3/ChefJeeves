503ad4333e401274af5955d00b01bb88974fd420
GARIMAKAUL <gkaul25@gmail.com>
2017-03-29 15:52:18 -0400

    Forgot password feature added

---
+++ ChefJeeves/protected/ChangePassword.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ChangePassword.aspx.cs" Inherits="ChefJeeves.ChangePassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">Change Password</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="heading" runat="server">Change Password</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="content" runat="server">
+    <form runat="server">
+        <asp:Label ID="lblNewPassword" runat="server" Text="New Password:"></asp:Label>
+        <asp:TextBox ID="txtNewPassword" runat="server" TextMode="Password"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" 
+            ControlToValidate="txtNewPassword" ErrorMessage="Can't be empty" 
+            Display="Dynamic" ForeColor="DarkRed"></asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="lblConfirmNewPassword" runat="server" Text="Confirm New Password:" ></asp:Label>
+        <asp:TextBox ID="txtConfirmNewPassword" runat="server" TextMode="Password"></asp:TextBox>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" 
+            ControlToCompare="txtNewPassword" ControlToValidate="txtConfirmNewPassword" 
+            ErrorMessage="Must be the same as above" ForeColor="DarkRed"></asp:CompareValidator>
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" CssClass="btn btn-default"/>
+        <asp:Button ID="btnCancel" runat="server" Text="Cancel" OnClick="btnReset_Click" CssClass="btn btn-default"/>
+        <br />
+        <asp:Label ID="lblError" runat="server" ForeColor="DarkRed"></asp:Label>
+    </form>
+</asp:Content>
---
+++ ChefJeeves/protected/ChangePassword.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web.Configuration;
+
+namespace ChefJeeves
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        private MySqlConnection con;
+        private MySqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+            String con_string = WebConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+            con = new MySqlConnection(con_string);
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "UpdatePassword";
+            cmd.Parameters.Add("User", MySqlDbType.VarChar, 64);
+            cmd.Parameters["User"].Value = Session["username"].ToString();
+            cmd.Parameters["User"].Direction = ParameterDirection.Input;
+            cmd.Parameters.Add("Pass", MySqlDbType.VarChar, 512);
+            cmd.Parameters["Pass"].Value = txtNewPassword.Text.Trim();
+            cmd.Parameters["Pass"].Direction = ParameterDirection.Input;
+            using (con)
+            {
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Redirect("~/Login.aspx");
+                }
+                catch (Exception ex)
+                {
+                    lblError.Text = ex.Message;
+                    con.Close();
+                }
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
